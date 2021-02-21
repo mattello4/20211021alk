22b842aad68d3daac0fde2948d2fddf52cd301da
Mateusz Paszkiewicz <mateusz.paszkiewicz@onet.pl>
2021-02-21 11:58:03 +0100

    Apk

---
+++ app.js
@@ -0,0 +1,34 @@
+const fromAnotherFile = require("./functions")
+const express = require("express");
+const app = express();
+const port = 3000;
+//kod JS
+//ustawienie, ze moja aplikacja musi korzystac z silnika hbs
+app.set("view engine", 'hbs')
+//wyrenderowanie strony, gdy uzytkownik wchodzi na stronę
+app.get('/', function (req, res) {
+const sample = () => {
+return 'proba'
+}
+res.render('index', {
+pageTitle: "Lekcja01",
+subTitle: fromAnotherFile.someTitle,
+anotherTitle: sample()
+})
+})
+//wypisanie na stronie tekstu
+app.get('/about', function (req, res) {
+res.send("My site")
+})
+// Podpięcie css
+var path = require('path')
+app.use('/assets', express.static(path.join(__dirname, "./assets")));
+//podpięcie js
+app.use('/js', express.static(path.joisssn(__dirname, "./js")));
+//server
+app.listen(port, (err) => {
+if (err) {
+return console.log("coś poszło nie tak...:", err)
+}
+console.log("serwer działa na porcie", port)
+})
---
+++ js/func.js
@@ -0,0 +1,13 @@
+const example = (e) => {
+    e.preventDefault();
+    const fromInput = document.getElementById('example').value;
+    document.getElementsByTagName('p')[0].innerText = fromInput;
+    }
+    document.getElementById('send').addEventListener("click", example);
+
+    const someTitle = "example example" //kod do dopisania
+module.exports = {
+sayHello,
+add,
+someTitle //kod do dopisania
+}
